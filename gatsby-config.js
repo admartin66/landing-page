0916b1cb9feb58d44ad244248da8b7f4cc6e4823
admartin66 <admartin66@gmail.com>
2018-12-24 12:11:29 -0500

    Revert "Update gatsby-config.js"
    This reverts commit 62110b754f7b0cc813544f46eb5c193d561c5b98.

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,14 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-     
+     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-131201429-1",
+        head: true,
+ 
+      },
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline'
   ],
