aca71b173153d4f10fd3a66b69ace74781eecc20
admartin66 <admartin66@gmail.com>
2018-12-24 11:31:29 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,20 +22,8 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-131201429-1",
-        // Puts tracking script in the head instead of the body
         head: true,
-        // Setting this parameter is optional
-        //anonymize: false,
-        // Setting this parameter is also optional
-        //respectDNT: false,
-        // Avoids sending pageview hits from custom paths
-        //exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Any additional create only fields (optional)
-        //sampleRate: 5,
-        //siteSpeedSampleRate: 10,
-        //cookieDomain: "example.com",
+ 
       },
     },
     'gatsby-plugin-sass',
